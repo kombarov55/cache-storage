60a0dc2bf7bfbe537a74e8b92f743ab6a8103a13
na_kombarov <kombarov55@gmail.com>
2018-03-07 16:52:59 +0300

    Добавлено начальное сохранение в файловую систему

--- src/com/company/FilesystemCacheStorage.java
+++ src/com/company/FilesystemCacheStorage.java
@@ -1,23 +1,46 @@
 package com.company;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class FilesystemCacheStorage implements CacheStorage {
 
-    private static final String filename = "cache";
     private final boolean persistent;
     private final String cacheDir;
+
+    private Marshaller marshaller = new Marshaller();
 
     FilesystemCacheStorage(boolean persistent, String cacheDir) {
         this.persistent = persistent;
         this.cacheDir = cacheDir;
+
+        createDirIfNeeded();
     }
 
     @Override
     public void put(Object obj) {
+        String data = marshaller.marshalize(obj);
 
+        try {
+            writeToFileStorage(data, obj.hashCode());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Object get(int hashcode) {
+        try {
+            String data = readFromFileStorage(hashcode);
+
+            return marshaller.demarshalize(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
@@ -29,5 +52,23 @@ public class FilesystemCacheStorage implements CacheStorage {
     @Override
     public void clear() {
 
+    }
+
+    private void createDirIfNeeded() {
+        File dir = new File(cacheDir);
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+    }
+
+    private void writeToFileStorage(String data, int hashcode) throws IOException {
+        Files.write(
+                Paths.get(cacheDir + "/" + hashcode),
+                data.getBytes(),
+                StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+    }
+
+    private String readFromFileStorage(int hashcode) throws IOException {
+        return Files.readAllLines(Paths.get(cacheDir + "/" + hashcode)).get(0);
     }
 }
---
+++ src/com/company/Marshaller.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.lang.reflect.Field;
+
+/**
+ * Маршализация / демаршализация объектов
+ */
+public class Marshaller {
+
+    public String marshalize(Object object) {
+        StringBuilder sb = new StringBuilder();
+
+        try {
+
+            for (Field field : object.getClass().getDeclaredFields()) {
+                field.setAccessible(true);
+                sb
+                        .append(field.getName())
+                        .append("=")
+                        .append(field.get(object));
+
+                sb.append("\t");
+
+            }
+
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return sb.toString();
+    }
+
+    public Object demarshalize(String str) {
+        return new Object();
+    }
+
+
+
+}
